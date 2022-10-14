7d85f9e8419f163b771a0cc9d1a24a1c99ba23f1
Jemi Jose <63700322+jemi-droid@users.noreply.github.com>
2022-10-14 13:00:30 +0530

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -6,7 +6,7 @@ import (
     "io/ioutil"
     "net/http"
 
-    "github.com/<YOUR_GITHUB_USER>/golang-auth0-example/todo"
+    "github.com/jemi-droid/go-angular/todo"
     "github.com/gin-gonic/gin"
 )
 
@@ -40,4 +40,29 @@ func CompleteTodoHandler(c *gin.Context) {
     todoItem, statusCode, err := convertHTTPBodyToTodo(c.Request.Body)
     if err != nil {
         c.JSON(statusCode, err)
-        retu
+        return
+    }
+    if todo.Complete(todoItem.ID) != nil {
+        c.JSON(http.StatusInternalServerError, err)
+        return
+    }
+    c.JSON(http.StatusOK, "")
+}
+
+func convertHTTPBodyToTodo(httpBody io.ReadCloser) (todo.Todo, int, error) {
+    body, err := ioutil.ReadAll(httpBody)
+    if err != nil {
+        return todo.Todo{}, http.StatusInternalServerError, err
+    }
+    defer httpBody.Close()
+    return convertJSONBodyToTodo(body)
+}
+
+func convertJSONBodyToTodo(jsonBody []byte) (todo.Todo, int, error) {
+    var todoItem todo.Todo
+    err := json.Unmarshal(jsonBody, &todoItem)
+    if err != nil {
+        return todo.Todo{}, http.StatusBadRequest, err
+    }
+    return todoItem, http.StatusOK, nil
+}
